a2f51c409cbc9424d94f563977fd34607a5bfe20
N777313 <143112474+N777313@users.noreply.github.com>
2026-07-25 03:35:51 +0500

    Update script.js

--- script.js
+++ script.js
@@ -5,26 +5,17 @@ const API_URL = "https://4d97-212-46-56-10.ngrok-free.app/products/";
 
 async function loadProducts() {
 
-    console.log("START LOAD");
-
-
-    const response = await fetch(API_URL);
-
-    console.log("STATUS:", response.status);
-
+    const response = await fetch(API_URL, {
+        headers: {
+            "ngrok-skip-browser-warning": "true"
+        }
+    });
 
     const products = await response.json();
 
-    console.log("PRODUCTS:", products);
-
-
     const container = document.getElementById("products");
 
-    console.log("CONTAINER:", container);
-
-
     container.innerHTML = "";
-
 
     products.forEach(product => {
 
@@ -49,6 +40,5 @@ async function loadProducts() {
     });
 
 }
-
 
 loadProducts();
