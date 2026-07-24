892f99cb9306603aacf892488357d426f5251dde
N777313 <143112474+N777313@users.noreply.github.com>
2026-07-25 03:30:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 console.log("SCRIPT LOADED 777");
+
 const API_URL = "https://4d97-212-46-56-10.ngrok-free.app/products/";
 
 
 async function loadProducts() {
 
+    console.log("START LOAD");
+
+
     const response = await fetch(API_URL);
+
+    console.log("STATUS:", response.status);
+
 
     const products = await response.json();
 
+    console.log("PRODUCTS:", products);
 
-    const container =
-        document.getElementById("products");
+
+    const container = document.getElementById("products");
+
+    console.log("CONTAINER:", container);
 
 
     container.innerHTML = "";
@@ -18,31 +28,19 @@ async function loadProducts() {
 
     products.forEach(product => {
 
-
         container.innerHTML += `
 
         <div class="product">
 
-            <h2>
-                ${product.name}
-            </h2>
+            <h2>${product.name}</h2>
 
+            <p>${product.description ?? ""}</p>
 
-            <p>
-                ${product.description ?? ""}
-            </p>
-
-
-            <h3>
-                $${product.price}
-            </h3>
-
+            <h3>$${product.price}</h3>
 
             <p>
-                Category:
-                ${product.category ?? ""}
+                Category: ${product.category ?? ""}
             </p>
-
 
         </div>
 
